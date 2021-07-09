f3639dbf692da4c71bfd9d814108cd21f1687b2a
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-09 19:40:10 +0530

    Create break.cpp

---
+++ break.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+   int i;
+
+   int n;
+
+   cin>>n;
+
+   for(i=1; i<=100; i++)
+
+   {
+
+      if(i == n) break;
+
+      else cout<<i<<endl;
+
+   }
+
+
+   return 0;
+
+}
